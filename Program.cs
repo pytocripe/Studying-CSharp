67f4b95fb19b959a3503f25ed1325207c802e33d
pytocripe <marat.moroz14@gmail.com>
2022-11-24 14:56:17 +0300

    Fourth program. A simple calculator for two number.

--- Program.cs
+++ Program.cs
@@ -10,19 +10,67 @@ namespace _1
     {
         static void Main(string[] args)
         {
-            // Checking a number for evenness. The second homework assignment from #SimpleCode.
+            //A simple calculator for two number. The third homework assignment from #SimpleCode.
 
-            Console.Write("Write a number to check for evenness: ");
-            int number = Convert.ToInt32(Console.ReadLine());
+            while (true)
+            {
+                double FirstNumber = 0;
+                double SecondNumber = 0;
 
-            if (number % 2 == 0)
-            {
-                Console.WriteLine("the number is even");
+                try
+                {
+                    Console.Write("Write first number: ");
+                     FirstNumber = Convert.ToDouble(Console.ReadLine());
+                    Console.Write("Write second number: ");
+                    SecondNumber = Convert.ToDouble(Console.ReadLine());
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Is not a number");
+                    continue;
+                }
+
+                Console.Write("Select action: '+' '-' '*' '/': ");
+                string action = Console.ReadLine();
+
+                switch (action)
+                {
+                    case "+":
+                        Console.WriteLine("The sum of the number " + FirstNumber + " and " + SecondNumber + " is " + (FirstNumber + SecondNumber));
+                        break;
+                    case "-":
+                        Console.WriteLine("The difference of the number " + FirstNumber + " and " + SecondNumber + " is " + (FirstNumber - SecondNumber));
+                        break;
+                    case "/":
+                        if (SecondNumber == 0)
+                        {
+                            Console.WriteLine("You can't divide by zero.");
+                            Console.ReadKey();
+                            Console.Clear();
+                            continue;
+                        }
+                      Console.WriteLine("The divide of the number " + FirstNumber + " by " + SecondNumber + " is " + (FirstNumber / SecondNumber));
+                        break;
+
+                    case "*":
+                     Console.WriteLine("The multiply of the number " + FirstNumber + " by " + SecondNumber + " is " + (FirstNumber * SecondNumber));
+                        break;
+                    default:
+                        Console.WriteLine("It is not an arithmetic action");
+                        break;
+
+                }
+
+                Console.ReadKey();
+                Console.Clear();
+
+
+
             }
-            else
-            {
-                Console.WriteLine("the number is not even");
-            }
+            
+
+
+
         }
     }
 }
